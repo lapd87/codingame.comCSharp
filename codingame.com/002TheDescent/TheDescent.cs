8bf262f7035f8c5696d9eeb73df946de6b02b1c8
lapd87 <mario87lapd@gmail.com>
2017-11-06 18:15:37 +0200

    adding problems 001-003

---
+++ codingame.com/002TheDescent/TheDescent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+/**
+ * The while loop represents the game.
+ * Each iteration represents a turn of the game
+ * where you are given inputs (the heights of the mountains)
+ * and where you have to print an output (the index of the mountain to fire on)
+ * The inputs you are given are automatically updated according to your last actions.
+ **/
+class Player
+{
+    static void Main(string[] args)
+    {
+
+        // game loop
+        while (true)
+        {
+            int maxHeight = int.MinValue;
+            int mountainNum = -1;
+
+            for (int i = 0; i < 8; i++)
+            {
+                int mountainH = int.Parse(Console.ReadLine()); // represents the height of one mountain.
+                if (maxHeight < mountainH)
+                {
+                    maxHeight = mountainH;
+                    mountainNum = i;
+                }
+
+                // Write an action using Console.WriteLine()
+                // To debug: Console.Error.WriteLine("Debug messages...");
+            }
+
+            Console.WriteLine(mountainNum); // The index of the mountain to fire on.
+
+        }
+    }
+}
